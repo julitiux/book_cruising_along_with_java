6272dacd9ba4012d9b0efee785556c28444d1303
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-07-30 00:45:15 -0600

    add two list: justOne and nothingHere

--- src/main/java/com/book_cruising_along_with_java/chapter_01/GenericsTypeInference.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_01/GenericsTypeInference.java
@@ -1,4 +1,11 @@
 package com.book_cruising_along_with_java.chapter_01;
 
+import java.util.Collections;
+import java.util.List;
+
 public class GenericsTypeInference {
+
+  List<String> justOne = Collections.singletonList("howdy");
+  List<String> nothingHere = Collections.emptyList();
+
 }
