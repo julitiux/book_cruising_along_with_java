2ac8d410c454d7ff845a9861cdafa5445793e1b4
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-07-30 00:51:27 -0600

    ERROR: incompatible types: Object cannot be converted to int

--- src/main/java/com/book_cruising_along_with_java/chapter_01/GenericsTypeWitness.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_01/GenericsTypeWitness.java
@@ -1,4 +1,19 @@
 package com.book_cruising_along_with_java.chapter_01;
 
+import java.util.function.Consumer;
+
 public class GenericsTypeWitness {
+
+  public <T> void process(Consumer<T> consumer) {
+  }
+
+  public static void display(int value) {
+  }
+
+  public static void main(String[] args) {
+
+    GenericsTypeWitness instance = new GenericsTypeWitness();
+    instance.process(input -> display(input)); // ERROR
+    //ERROR: incompatible types: Object cannot be converted to int
+  }
 }
