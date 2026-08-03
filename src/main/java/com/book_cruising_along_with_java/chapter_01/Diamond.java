e101359bb7f10aba9a7754cf192d757a4e5330f7
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-02 23:53:46 -0600

    using the diamond operator like java 7

--- src/main/java/com/book_cruising_along_with_java/chapter_01/Diamond.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_01/Diamond.java
@@ -1,4 +1,11 @@
 package com.book_cruising_along_with_java.chapter_01;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Diamond {
+
+  Map<String, List<Integer>> score = new HashMap<String, List<Integer>>();
+
 }
