12970e6bbf22a913ff8c2d86cc3fff1e07c3bbdc
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-03 00:07:09 -0600

    implement an anonymous inner class

--- src/main/java/com/book_cruising_along_with_java/chapter_01/Diamond.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_01/Diamond.java
@@ -7,7 +7,9 @@ import java.util.Map;
 public class Diamond {
 
 //  Map<String, List<Integer>> score = new HashMap<String, List<Integer>>();
+//  Map<String, List<Integer>> score = new HashMap<>();
 
-  Map<String, List<Integer>> score = new HashMap<>();
+  Map<String, List<Integer>> score = new HashMap<>(){
 
+  };
 }
