29db3e725caf6567025d9cf20f370f95a99b8e36
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-03 00:13:28 -0600

    iterate over a collection of number and print the double of echa value

--- src/main/java/com/book_cruising_along_with_java/chapter_01/Lambda.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_01/Lambda.java
@@ -1,4 +1,13 @@
 package com.book_cruising_along_with_java.chapter_01;
 
+import java.util.List;
+
 public class Lambda {
+
+  List<Integer> numbers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+  public void print() {
+
+    numbers.forEach((Integer number) -> System.out.println(number * 2));
+  }
 }
