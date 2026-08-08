63a44de3474e86d954babc66fc8fc3ea99478708
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-07 23:51:06 -0600

    used type inference for the parameter of the lambda expression passed to the comparing() method.

--- src/main/java/com/book_cruising_along_with_java/chapter_01/LambdaTypeInferenceFail.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_01/LambdaTypeInferenceFail.java
@@ -1,4 +1,16 @@
 package com.book_cruising_along_with_java.chapter_01;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class LambdaTypeInferenceFail {
+
+  List<String> languages = List.of("Java", "Kotlin", "Scala", "Groovy", "Clojure", "JRuby");
+
+  public void printLanguages() {
+
+    languages.stream()
+      .sorted(Comparator.comparing(name -> name.length()))
+      .forEach(System.out::println);
+  }
 }
