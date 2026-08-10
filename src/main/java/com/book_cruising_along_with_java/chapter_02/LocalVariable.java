b276ad13fbf0f3226b335b2b3d45d8f8f9ac94d6
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-09 23:16:19 -0600

    the compiler figures out that the variable is of type String based on whats assigned to it and doesnt need us to specify the type explicity

--- src/main/java/com/book_cruising_along_with_java/chapter_02/LocalVariable.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_02/LocalVariable.java
@@ -3,9 +3,8 @@ package com.book_cruising_along_with_java.chapter_02;
 public class LocalVariable {
 
   public static void greet(){
-
-    String  message = "Hello there";
-
+    var message = "Hello there";
+    
     System.out.println(message);
   }
 }
