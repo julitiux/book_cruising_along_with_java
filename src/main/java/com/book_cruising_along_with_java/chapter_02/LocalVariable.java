a549e8ee7bc2fb0b66a63caa081bf877274d2b0e
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-09 23:14:38 -0600

    providing explicit type information for the message variable

--- src/main/java/com/book_cruising_along_with_java/chapter_02/LocalVariable.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_02/LocalVariable.java
@@ -1,4 +1,11 @@
 package com.book_cruising_along_with_java.chapter_02;
 
 public class LocalVariable {
+
+  public static void greet(){
+
+    String  message = "Hello there";
+
+    System.out.println(message);
+  }
 }
