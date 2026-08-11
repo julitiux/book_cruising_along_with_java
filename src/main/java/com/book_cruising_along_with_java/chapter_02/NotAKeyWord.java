1b5ad28f0210e5284037f2c9544c41cb6d0f23d2
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-10 19:12:04 -0600

    defining variables with the same var isn't a good idea unless your intention is to mess with the minds of your fellow programmers

--- src/main/java/com/book_cruising_along_with_java/chapter_02/NotAKeyWord.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_02/NotAKeyWord.java
@@ -1,4 +1,12 @@
 package com.book_cruising_along_with_java.chapter_02;
 
 public class NotAKeyWord {
+
+  void NotAKeyWord() {
+    var PI = Math.PI;
+    String var = "please don't"; // Possible, but not a good idea
+    //var var = "please don't";  // Also possible, but avoid
+
+    System.out.println(var);     // prints: please don't
+  }
 }
