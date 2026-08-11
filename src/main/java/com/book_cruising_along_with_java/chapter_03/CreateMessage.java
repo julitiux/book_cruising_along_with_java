4e64b8dc63974ef01c96a5f7a9bc79d4cf1168e3
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-10 23:53:51 -0600

    the refactored version produces exactly teh same output as the noisy version, but the code is easier to read and doesn't use +=

--- src/main/java/com/book_cruising_along_with_java/chapter_03/CreateMessage.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_03/CreateMessage.java
@@ -2,16 +2,18 @@ package com.book_cruising_along_with_java.chapter_03;
 
 public class CreateMessage {
 
-  public static String createMessage(){
+  public static String createMessage() {
 
-    String message = "Thank you for your purchase.";
-    message += " We hope you had a pleasant experience.\n\n";
-    message += "We request that you take a few minutes ";
-    message += "to provide your feedback.\n\n";
-    message += "Please fill out the survey at https://survey.example.com\n\n";
-    message += "If you have any questions or comments, ";
-    message += "please click on the \"Support\" link\n";
-    message += "at https://www.example.com.\n";
+    var message = """
+      Thank you for your purchase. We hope you had a pleasant experience.
+      
+      We request that you take a few minutes to provide your feedback.
+      
+      Please fill out the survey at https://survey.example.com
+      
+      If you have any questions or comments, please click on the "Support" link
+      at https://www.example.com.
+      """;
 
     return message;
   }
