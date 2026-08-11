b30031a4274d77073ef5dd361e5888bdb95833fd
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-10 19:07:13 -0600

    compile the code and use the javap tool to view the bitecode that was generated by the compiler

--- src/main/java/com/book_cruising_along_with_java/chapter_02/TypeInferred.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_02/TypeInferred.java
@@ -1,4 +1,11 @@
 package com.book_cruising_along_with_java.chapter_02;
 
 public class TypeInferred {
+
+  public static void main(String[] args){
+
+    var message = "hello there";
+    var max = 1000;
+    var instance = new TypeInferred();
+  }
 }
