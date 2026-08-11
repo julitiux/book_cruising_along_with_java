a93f8d414785a87ebad65867e7f11c9e9a058f12
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-11 16:53:32 -0600

    the code use += to append the text to the String instance

--- src/main/java/com/book_cruising_along_with_java/chapter_03/CreateMessage.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_03/CreateMessage.java
@@ -4,16 +4,14 @@ public class CreateMessage {
 
   public static String createMessage() {
 
-    var message = """
-      Thank you for your purchase. We hope you had a pleasant experience.
-      
-      We request that you take a few minutes to provide your feedback.
-      
-      Please fill out the survey at https://survey.example.com
-      
-      If you have any questions or comments, please click on the "Support" link
-      at https://www.example.com.
-      """;
+    String message = "Thank you for your purchase.";
+    message += " We hope you had a pleasant experience.\n\n";
+    message += "We request that you take a few minutes ";
+    message += "to provide your feedback.\n\n";
+    message += "Please fill out the survey at https://survey.example.com\n\n";
+    message += "If you have any questions or comments, ";
+    message += "please click on the \"Support\" link\n";
+    message += "at https://www.example.com.\n";
 
     return message;
   }
