caace42b3f8791a82304a4b50cc5dad8574e7137
Julio Ramirez <rrodriguez.julio@gmail.com>
2026-08-11 23:20:29 -0600

    the refactored version produces exactly teh same output as the noisy version, but the code is easier to read and doesn't use +=

--- src/main/java/com/book_cruising_along_with_java/chapter_03/CreateMessageConcise.java
+++ src/main/java/com/book_cruising_along_with_java/chapter_03/CreateMessageConcise.java
@@ -1,4 +1,18 @@
 package com.book_cruising_along_with_java.chapter_03;
 
 public class CreateMessageConcise {
+
+  public static String createMessage() {
+    var message = """
+        Thank you for your purchase. We hope you had a pleasant experience.
+        
+        We request that you take a few minutes to provide your feedback.
+        
+        Please fill out the survey at https://survey.example.com
+        
+        If you have any questions or comments, please click on the "Support" link
+        at https://www.example.com.
+        """;
+    return message;
+  }
 }
